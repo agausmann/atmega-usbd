5f1a589015dd77eed4124f3606d3804d50c6ecc0
rmsyn <rmsynchls@gmail.com>
2023-04-22 04:02:13 +0000

    examples: update example to pass the PLL register
    Adds passing the PLL register into the USB struct for PLL enable/disable
    in the UsbBus impl.

--- examples/arduino_keyboard.rs
+++ examples/arduino_keyboard.rs
@@ -52,7 +52,7 @@ fn main() -> ! {
 
     let usb_bus = unsafe {
         static mut USB_BUS: Option<UsbBusAllocator<UsbBus>> = None;
-        &*USB_BUS.insert(UsbBus::new(usb))
+        &*USB_BUS.insert(UsbBus::new(usb, pll))
     };
 
     let hid_class = HIDClass::new(&usb_bus, KeyboardReport::desc(), 1);
