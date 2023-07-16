f0560e956c9a94e4203162e2fb280f39ea66c147
Adam Gausmann <adam@gaussian.dev>
2023-07-15 21:52:29 -0500

    Move arduino_keyboard documentation to the source file

--- examples/arduino_keyboard.rs
+++ examples/arduino_keyboard.rs
@@ -1,3 +1,23 @@
+//! A "Hello World" example that can be run on an Arduino Leonardo.
+//!
+//! # Usage
+//!
+//! 1. (Optional) Connect a pushbutton switch to the D2 pin of the Leonardo, and
+//! connect the other pin of the switch to GND.
+//!
+//! 2. Connect the Leonardo to the computer with a USB cable.
+//!
+//! 3. Make sure [Ravedude](https://github.com/Rahix/avr-hal/tree/main/ravedude)
+//! is installed. Then "run" the example to deploy it to the Arduino:
+//!
+//!   ```
+//!   cargo run --release --example arduino_keyboard
+//!   ```
+//!   
+//! 4. Open Notepad (or whatever editor or text input of your choosing). Press
+//! the button (or if you are not using one, short D2 to GND with a jumper). You
+//! should see it type "Hello World"
+
 #![no_std]
 #![cfg_attr(not(test), no_main)]
 #![feature(lang_items)]
